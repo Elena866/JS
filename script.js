392da1947cf6456a4034aa662e314e43e11a8dba
Елена Аксенова <999900009999@mail.ru>
2020-09-21 21:59:27 +0300

    added js

---
+++ script.js
@@ -0,0 +1,4 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart');
